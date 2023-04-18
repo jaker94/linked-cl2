f7bdd1b91aacfbf5c849dc76713a290eb94b4deb
Jacob <jakeregister94@gmail.com>
2023-04-17 19:08:27 -0600

    unresolved error 403 on home page using next.js image 26:21

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,8 +5,16 @@ function Home() {
   return (
     <div>
       <header>
-        <div className="">
-          <Image src="https://rb.gy/vtbzlp" layout="fill" objectFit="contain" priority={1} alt=""/>
+        <div className="relative w-36 h-10">
+          <Image
+            src="https://rb.gy/vtbzlp"
+            priority={1}
+            alt=""
+            fill
+            sizes="100vw"
+            style={{
+              objectFit: "contain"
+            }} />
         </div>
       </header>
     </div>
