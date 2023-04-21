b4b5ba2d721a7964a9717ede82bbc6c5d6eaba5a
Jacob <jakeregister94@gmail.com>
2023-04-21 15:20:28 -0600

    authentication errors removed. 0ath, adapter, and mongodb working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-
+import { signOut } from "next-auth/react"
 import Head from 'next/head'
 
 export default function Home() {
@@ -8,7 +8,7 @@ export default function Home() {
         <title>LinkedIn</title>
         <link rel="icon" href="favicon.ico" />
       </Head>
-      Linkedin Clone
+      <button onClick={signOut}>Sign Out</button>
     </div>
   )
 }
