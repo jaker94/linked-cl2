ff2a3a0da491b97eb1f8f6ec8427cd8e6ae1d369
Jacob <jakeregister94@gmail.com>
2023-04-24 18:44:55 -0600

    reusable header complete, sidebar nearly completed but session functionality not added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,24 @@
 import { signOut } from "next-auth/react"
 import Head from 'next/head'
 import Header from "../components/Header"
+import Sidebar from "../components/Sidebar"
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[#f3f2ef] dark:bg-black dark:text-white h-screen
+    overflow-y-scroll md:space-y-6">
       <Head>
-        <title>LinkedIn</title>
+        <title>Feed | LinkedIn</title>
         <link rel="icon" href="favicon.ico" />
       </Head>
       <Header/>
+      <main className="flex justify-center gap-x-4 px-4 sm:px-12">
+        <div className="flex flex-col md:flex-row gap-5">
+        <Sidebar />
+          {/* feed */}
+        </div>
+        {/* widget */}
+      </main>
     </div>
   )
 }
